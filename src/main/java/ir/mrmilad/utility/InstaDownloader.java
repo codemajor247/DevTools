20bfd4e10644a9a8ce9915f4b41969ff44be8079
Milad Seifoori [Android Developer] <milad.s@mabna.com>
2018-06-06 21:54:51 +0430

    hotfix

--- src/main/java/ir/mrmilad/utility/InstaDownloader.java
+++ src/main/java/ir/mrmilad/utility/InstaDownloader.java
@@ -1,6 +1,6 @@
 package ir.mrmilad.utility;
 
-import android.content.Context;
+import android.os.AsyncTask;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -11,46 +11,86 @@ public class InstaDownloader {
 
     public static final int Video = 0;
     public static final int Photo = 1;
+    int ResponseCode = 0;
+    Exception exceptionRes;
 
     onCheck listener;
+    String instaUrl;
 
     public interface onCheck {
+        void onChecking();
         void onComplete(int type, String url);
+        void onFailed(Exception ex);
     }
 
-    public void setOnCheck(onCheck status) {
+    public void setOnCheck(String postUrl, onCheck status) {
+        this.instaUrl = postUrl;
         this.listener = status;
+        new onFetch().execute();
     }
 
-    public void checkUrl(String url) {
-        String type = "";
-        String html = WebClient.DownloadUrlString(url, "", true);
-        Document doc = Jsoup.parse(html);
-        Elements metaTags = doc.getElementsByTag("meta");
-        for (Element metaTag : metaTags) {
-            String content = metaTag.attr("content");
-            String name = metaTag.attr("property");
-            if (name.equals("og:type")) {
-                type = content;
+    public class onFetch extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                listener.onChecking();
+                String html = WebClient.DownloadUrlString(instaUrl, "", true);
+                ResponseCode = 200;
+                return html;
+            } catch (Exception ex) {
+                exceptionRes = new Exception();
+                exceptionRes = ex;
+                ResponseCode = 400;
+                return "err." + ex;
             }
         }
-        if (type.contains("photo") || type.contains("image")) {
-            for (Element metaTag : metaTags) {
-                String content = metaTag.attr("content");
-                String name = metaTag.attr("property");
-                if (name.equals("og:image")) {
-                    listener.onComplete(Photo, content);
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            if (ResponseCode == 200) {
+                try {
+                    String type = "";
+                    Document doc = Jsoup.parse(result);
+                    Elements metaTags = doc.getElementsByTag("meta");
+                    for (Element metaTag : metaTags) {
+                        String content = metaTag.attr("content");
+                        String name = metaTag.attr("property");
+                        if (name.equals("og:type")) {
+                            type = content;
+                        }
+                    }
+                    if (type.contains("photo") || type.contains("image")) {
+                        for (Element metaTag : metaTags) {
+                            String content = metaTag.attr("content");
+                            String name = metaTag.attr("property");
+                            if (name.equals("og:image")) {
+                                listener.onComplete(Photo, content);
+                            }
+                        }
+                    } else {
+                        for (Element metaTag : metaTags) {
+                            String content = metaTag.attr("content");
+                            String name = metaTag.attr("property");
+                            if (name.equals("og:video")) {
+                                listener.onComplete(Video, content);
+                            }
+                        }
+                    }
+                } catch (Exception e) {
+                    listener.onFailed(e);
                 }
-            }
-        } else {
-            for (Element metaTag : metaTags) {
-                String content = metaTag.attr("content");
-                String name = metaTag.attr("property");
-                if (name.equals("og:video")) {
-                    listener.onComplete(Video, content);
-                }
+
+            } else {
+                listener.onFailed(exceptionRes);
             }
         }
+
     }
 
 }
